71a42cc21fa2dfae654eb540c4a7755a2cded643
cow_moo <h_hojin@naver.com>
2019-11-07 17:08:47 +0900

    init file commit

---
+++ src/algorithm/study/AlgorithmStudy.java
@@ -0,0 +1,10 @@
+package algorithm.study;
+
+public class AlgorithmStudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QuestionOne answerObject = new QuestionOne();
+		answerObject.answer();
+	}
+}
---
+++ src/algorithm/study/QuestionOne.java
@@ -0,0 +1,13 @@
+package algorithm.study;
+
+public class QuestionOne {
+	public void test()
+	{
+		System.out.println("hello");
+	}
+	
+	public void answer()
+	{
+		System.out.println("answer");
+	}
+}
