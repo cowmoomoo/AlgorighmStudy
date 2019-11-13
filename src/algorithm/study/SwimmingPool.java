008f68d54aaebe15b0c3fc7ffbd581900bd43715
cow_moo <h_hojin@naver.com>
2019-11-13 16:11:58 +0900

    solve siwmming problems

--- src/algorithm/study/SwimmingPool.java
+++ src/algorithm/study/SwimmingPool.java
@@ -1,68 +1,112 @@
 package algorithm.study;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-import java.util.ArrayList;
-
+// 삼성 sw 제출시에는 Solution 클래스로 제출 해야 함
+//public class Solution {
 public class SwimmingPool {
-	static int vertex, edge, S;
-	public void solution()
+	
+	private boolean inRange(int nextX, int nextY, int poolSize)
 	{
+		if ((0 > nextX) || (nextX >= poolSize) || (0 > nextY) || (nextY >= poolSize))
+			return false;
+		else
+			return true;
+	}
+	
+	public int solution(int[][] swimmingPool, int[] startPosition, int[] endPosition, int poolSize)
+	{
+		int[] swimDown = {1, 0};
+		int[] swimUp = {-1, 0};
+		int[] swimLeft = {0, -1};
+		int[] swimRight = {0, 1};
 		
-//		File path = new File(".");
-//		System.out.println(path.getAbsolutePath());
+		int[][] moveMent = {swimDown, swimUp, swimLeft, swimRight};
+		boolean[][] visited = new boolean[poolSize][poolSize];
 		
-		try {
-			//create file obj
-			File file = new File(".\\sample_input.txt");
-			
-			FileReader fileReader = new FileReader(file);
-			
-			BufferedReader bufReader = new BufferedReader(fileReader);
-			
-			String testCase = bufReader.readLine();
-			int numOfTestCase = Integer.parseInt(testCase);
-			for(int roop = 0; roop < numOfTestCase; roop++)
+		int answer = -1;
+		int depth = 0;
+		
+		Queue<int[]> q = new LinkedList<int[]>();
+		
+		q.add(startPosition);
+		visited[startPosition[0]][startPosition[1]] = true;
+		
+		while(!q.isEmpty())
+		{
+			int qSize = q.size();
+			for(int i = 0; i < qSize; i++)
 			{
-				int poolSize = Integer.parseInt(bufReader.readLine());
-				System.out.println(poolSize);
-				
-				ArrayList<String> adj = new ArrayList<>(); 
-				
-				for(int i = 0; i < poolSize; i++)
+				int[] currentPosition = q.poll();
+				if((currentPosition[0] == endPosition[0]) && (currentPosition[1] == endPosition[1]))
+					answer = depth;
+				for(int j = 0; j < moveMent.length; j++)
 				{
-					adj.add(bufReader.readLine());
-					
+					int nextX = moveMent[j][1] + currentPosition[0];
+					int nextY = moveMent[j][0] + currentPosition[1];
+					if(!this.inRange(nextX, nextY, poolSize))
+						continue;
+					if(swimmingPool[nextX][nextY] == 1)
+						continue;
+					if(!visited[nextX][nextY])
+					{
+						int[] nextPosition = {nextX, nextY};
+						q.add(nextPosition);
+						visited[nextX][nextY] = true;
+					}
 				}
-				String startPosition = bufReader.readLine();
-				String escapePosiont = bufReader.readLine();
-				System.out.println(startPosition);
-				System.out.println(escapePosiont);
-				
-				int moveCount = 0;
-				System.out.println(adj);
-				while(true)
-				{
-					// TODO exit
-					moveCount += 1;
-					if(moveCount == 2)
-						break;
-				}
-				System.out.println("#"+(roop+1)+" "+moveCount);
 			}
-			bufReader.close();
+			depth += 1;
 		}
-		catch (FileNotFoundException e)
+		return answer;
+	}
+	
+	public static void main(String[] args) 
+	{
+		// Solution swimmer = new solution();
+		SwimmingPool swimmer = new SwimmingPool();
+		
+		// InputStreamReader inputStream = new InputStreamReader(System.in);
+		Scanner sc = new Scanner(System.in);
+
+		//테스트 케이스 읽고 다음 줄
+		int testCase = sc.nextInt();
+		sc.nextLine();
+		
+		//결과 저장 용도
+		int[] results = new int[testCase];		
+		
+		for(int i = 0; i < testCase; i ++)
 		{
-			e.printStackTrace();
+			int poolSize = sc.nextInt();
+			int[][] swimmingPool = new int[poolSize][poolSize];
+			sc.nextLine();
+			for (int j = 0; j <poolSize; j++)
+			{
+				for (int k = 0; k< poolSize; k++)
+				{
+					swimmingPool[j][k] = sc.nextInt();
+				}
+			}
+			sc.nextLine();
+			int[] startPosition = new int[2];
+			startPosition[0] = sc.nextInt();
+			startPosition[1] = sc.nextInt();
+			
+			sc.nextLine();
+			int[] endPosition = new int[2];
+			endPosition[0] = sc.nextInt();
+			endPosition[1] = sc.nextInt();
+			
+			results[i] = swimmer.solution(swimmingPool, startPosition, endPosition, poolSize);
 		}
-		catch (IOException e)
+		
+		for(int i = 0; i< testCase; i++)
 		{
-			System.out.println(e);
+			System.out.println("#"+(i+1)+" "+results[i]);
 		}
+
 	}
 }
