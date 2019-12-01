68c4092382c1d1abdb5538d1aabfcada69412446
cow_notebook <h_hojin@naver.com>
2019-12-01 13:52:06 +0900

    try to solve the N & thievery problem

--- src/algorithmStudy/week4/Thievery.java
+++ src/algorithmStudy/week4/Thievery.java
@@ -1,25 +1,38 @@
 package algorithmStudy.week4;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
 
 public class Thievery {
-	static HashMap<Integer, Integer> cache = new HashMap<>();
 	
+	//인터넷 풀이
+	public int solution1(int[] money) {
+        int answer = 0;
+        int length = money.length;
+        int[] dp =new int[length-1];
+        int[] dp2= new int[length];
+        
+        dp[0]=money[0];
+        dp[1]=money[0];
+        dp2[0]=0;
+        dp2[1]=money[1];
+        for(int i=2;i<length-1;i++){
+            dp[i]=Math.max(dp[i-2]+money[i],dp[i-1]);
+        }
+        for(int i=2;i<length;i++){
+            dp2[i]=Math.max(dp2[i-2]+money[i],dp2[i-1]);
+        }
+        answer = Math.max(dp[length-2],dp2[length-1]);
+        System.out.println(answer);
+        return answer;
+    }
 	public int solution(int[] money) {
-		int answer = 0;
+		int answer = 1;
+		HashMap<Integer, Integer> mem1 = new HashMap<>();
+		HashMap<Integer, Integer> mem2 = new HashMap<>();
+		
+		mem1.put(0, money[0]);
+		mem1.put(1,  )
 		return answer;
-	}
-	public int thievery(int[] remainHouse) {
-		if(remainHouse.length == 0) { 
-			return 0;
-		} else if(remainHouse.length == 1) {
-			return Math.max(remainHouse[0], remainHouse[1]);
-		} else {
-			//TODO
-			return 0;
-		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
