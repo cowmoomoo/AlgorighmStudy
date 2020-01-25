c265c367953a91efe79ca8a6bdeb2a843ab522c5
cow_notebook <h_hojin@naver.com>
2020-01-25 16:15:50 +0900

    solve mockExam problem

--- src/algorithmStudy/week10/IslandConnect.java
+++ src/algorithmStudy/week10/IslandConnect.java
@@ -3,6 +3,7 @@ package algorithmStudy.week10;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.stream.IntStream;
+import java.util.Stack;
 
 class CostInfo implements Comparable<CostInfo>
 {
@@ -21,7 +22,7 @@ class CostInfo implements Comparable<CostInfo>
 	@Override
 	public int compareTo(CostInfo target)
 	{
-		// 보급량에 따라 오름차순 정렬
+		// 오름차순
 		if (this.weight > target.weight) {
 			return 1;
 		} else if (this.weight < target.weight) {
@@ -35,6 +36,7 @@ public class IslandConnect {
 	public int solution(int n, int[][] costs) {
 		int answer = 0;
 		PriorityQueue<CostInfo> priorityQueue = new PriorityQueue<CostInfo>();
+		int[][] adj = new int[n][n];
 		// System.out.println("init Q");
 		for(int[] costInfo : costs) {
 			// System.out.println(costInfo[0]+", "+costInfo[1]+", "+ costInfo[2]);
@@ -55,25 +57,82 @@ public class IslandConnect {
 //			CostInfo costInfo = priorityQueue.poll();
 //			System.out.println(costInfo.island1 +", "+costInfo.island2+", "+costInfo.weight);
 //		}
+		
 		int[] visited = new int[n];
 		Arrays.fill(visited, 0);
 		// System.out.println(Arrays.toString(visited));
-		while(true) {
+		while(!priorityQueue.isEmpty()) {
 			CostInfo costInfo = priorityQueue.poll();
-			System.out.println(costInfo.island1 +", "+costInfo.island2+", "+costInfo.weight);
-			visited[costInfo.island1] += 1;
-			visited[costInfo.island2] += 1;
-			System.out.println(Arrays.toString(visited));
-			answer = answer + costInfo.weight;
+			int island1 = costInfo.island1;
+			int island2 = costInfo.island2;
+			int weight = costInfo.weight;
 			
-			System.out.println(IntStream.of(visited).anyMatch(x -> x ==0));
+			System.out.println(island1);
+			System.out.println(island2);
+			
+			adj[island1][island2] = weight;
+			
+//			if(isNotLoop(adj, island1)) {
+			if (visited[island1] > 0 && visited[island2] > 0) {
+				continue;
+			}
+			if(visited[island1] == 0 || visited[island2] == 0) {
+				System.out.println("in");
+				System.out.println(island1 +", "+island2+", "+weight);
+				visited[island1] += 1;
+				visited[island2] += 1;
+				System.out.println(Arrays.toString(visited));
+			
+				answer = answer + weight;
+			}
+			else {
+				adj[island1][island2] = 0;
+			}
+			// System.out.println(IntStream.of(visited).anyMatch(x -> x ==0));
 			
 			if (!IntStream.of(visited).anyMatch(x -> x == 0)) {
 				break;
 			}
 		}
+		System.out.println(answer);
 		return answer;
 	}
+	
+	public boolean isNotLoop(int[][] adj, int startNode) {
+		
+		int nodeSize = adj.length;
+		boolean[] visited = new boolean[nodeSize];
+		Stack<Integer> stack = new Stack<Integer>();
+		visited[startNode] = true;
+		stack.add(startNode);
+		
+    	while(!stack.isEmpty()) {
+        	startNode = stack.pop();
+        	
+        	for(int endNode = 0; endNode < nodeSize; endNode++) {
+        		if (startNode == endNode)
+        			continue;
+        		if(adj[startNode][endNode] > 0) {
+        			if (visited[startNode] && visited[endNode])
+        			{
+        				System.out.println("loop??");
+        				System.out.println(startNode);
+        				System.out.println(endNode);
+        				return false;
+        			}	
+            		// 방문했으면 다음 노드 체크
+            		if (visited[endNode])
+            			continue;
+            		else {
+            			stack.add(endNode);
+                		visited[endNode] = true;
+            		}
+        		}           			
+        	}
+        }
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IslandConnect agent = new IslandConnect();
---
+++ src/algorithmStudy/week11/MockExam.java
@@ -0,0 +1,58 @@
+package algorithmStudy.week11;
+import java.util.Arrays;
+
+public class MockExam {
+	public int[] solution(int[] answers) {
+		int [] score = new int[3];
+		// 5
+		int [] pattern1 = {1, 2, 3, 4, 5};
+		// 8
+		int [] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
+		// 10
+		int [] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+		
+		int numOfPattern1 = pattern1.length;
+		int numOfPattern2 = pattern2.length;
+		int numOfPattern3 = pattern3.length;
+		
+		for(int idx =0, length = answers.length; idx < length; idx ++) {
+			int check = answers[idx];
+			if(check == pattern1[idx % numOfPattern1])
+				score[0] += 1;
+			if(check == pattern2[idx % numOfPattern2])
+				score[1] += 1;
+			if(check == pattern3[idx % numOfPattern3])
+				score[2] += 1;
+		}
+		int[] temp = score.clone();
+		// System.out.println(Arrays.toString(temp));
+		Arrays.sort(temp);
+		// System.out.println(Arrays.toString(temp));
+		int heighScore = temp[2];
+		// System.out.println(heighScore);
+		int cnt = 0;
+		for (int scoreIdx = 0; scoreIdx < 3; scoreIdx ++) {
+			if (heighScore == score[scoreIdx]) {
+				cnt += 1;
+			}
+		}
+		int[] answer = new int[cnt];
+		int idx = 0;
+		for (int scoreIdx = 0; scoreIdx < 3; scoreIdx ++) {
+			if (heighScore == score[scoreIdx]) {
+				answer[idx] = scoreIdx + 1;
+				idx += 1;
+			}
+		}
+		// System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] answers = {1, 2, 3, 4, 5};
+		MockExam agent = new MockExam();
+		agent.solution(answers);
+	}
+
+}
